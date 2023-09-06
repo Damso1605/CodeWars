6c22ab73b50b92af1a6d2c22563f9598fd3eb43f
Damso1605 <damso.sok@mail.ru>
2023-09-07 01:57:05 +0300

    Create UFC

---
+++ ufc.rb
@@ -0,0 +1,8 @@
+def quote(fighter)
+  case fighter.downcase
+  when 'george saint pierre'
+    'I am not impressed by your performance.'
+  when 'conor mcgregor'
+    "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+  end
+end
